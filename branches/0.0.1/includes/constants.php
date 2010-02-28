0087b83f004ab97e5b7659c9f98a98b0c6df9f6d
maidenfan <maidenfan@77e6e1be-d5c8-11de-9f34-657d07d5c07a>
2010-02-28 14:53:06 +0000

    0.0.1

---
+++ branches/0.0.1/includes/constants.php
@@ -0,0 +1,37 @@
+<?php
+/*
+ * $Id: constants.php 9 2010-02-19 12:57:09Z maidenfan $
+ * OpenBooking version 0.0.1
+ * Copyright (c) 2010 Craig Watson [ craig@cwatson.org ]
+ * Distributed Under the Mozilla Public License 1.1 [ http://www.mozilla.org/MPL/MPL-1.1.html ]
+ */
+
+if (!defined('IN_OB')){
+	exit;
+}
+
+// Version Number
+define('OB_VERSION','0.0.1');
+
+// Resource Restrictions
+define('RESTRICT_ON',   1);
+define('RESTRICT_OFF',  0);
+
+// 
+define('FORM_CHECKED',       1);
+define('FORM_NOT_CHECKED',   0);
+
+// Resource Advance Units
+define('ADVANCE_MONTH', 'm');
+define('ADVANCE_DAY',   'd');
+define('ADVANCE_HOUR',  'h');
+
+// Tables
+define('RESOURCES_TABLE',           $table_prefix . "resources");
+define('BOOKINGS_TABLE',            $table_prefix . "bookings");
+define('SLOTS_TABLE',               $table_prefix . "slots");
+define('RESOURCE_CATEGORIES_TABLE', $table_prefix . "resource_categories");
+
+?>
+
+
