a8753ca8337a1bc3561ba5b0304c45d42a031a84
maidenfan <maidenfan@77e6e1be-d5c8-11de-9f34-657d07d5c07a>
2010-03-09 15:43:40 +0000

    Clear-out and re-sort

---
+++ trunk/includes/slot_delete.php
@@ -0,0 +1,32 @@
+<?php
+/*
+ * $Id$
+ * OpenBooking version 0.0.1
+ * Copyright (c) 2010 Craig Watson [ craig@cwatson.org ]
+ * Distributed Under the Mozilla Public License 1.1 [ http://www.mozilla.org/MPL/MPL-1.1.html ]
+ */
+
+if (!defined('IN_OB')){
+	exit;
+}
+
+if (isset($_POST['submit'])) {
+
+    $id = addslashes(htmlspecialchars($_POST['id']));
+    $start = addslashes(htmlspecialchars($_POST['start']));
+    $end = addslashes(htmlspecialchars($_POST['end']));
+    $days = addslashes(htmlspecialchars($_POST['days']));
+
+    if($db->Execute("DELETE FROM " . SLOTS_TABLE . " WHERE slot_id = $id")){
+        echo "<p>You have successfully deleted the slot running from $start to $end on $days.</p>";
+    } else {
+        echo "<p class=\"error\">There was an error deleting the slot.</p>";
+    }
+    
+    echo "<p><small>[ <a href=\"javascript:hide_element('AJAX_update')\">Hide this message</a> ]</small></p>\n";
+
+} else {
+    echo '<p class="error">The form has not been submitted</p>';
+}
+
+?>
---
+++ trunk/javascript/ajax.js
@@ -0,0 +1,16 @@
+
+    /** JAVASCRIPT -- Custom wrapper functions for the AJAX library **/
+    
+function ajax_update(update_id, update_url){
+    var ajax = new Ajax.Updater(update_id, update_url, {method: 'get'});
+    document.getElementById(update_id).style.display = 'block';
+}
+
+function ajax_form_submit(form_id, update_id, submit_url, after, after_update, after_url){
+
+    // Generate the post text and do the query 
+    var post = generate_post(form_id);
+    var ajax = new Ajax.Updater(update_id, submit_url, {method: 'post', parameters: post, onSuccess: function() { ajax_update(after_update,after_url); new Effect.BlindDown(after_update);} } );
+    document.getElementById(update_id).style.display = 'block';
+       
+}
