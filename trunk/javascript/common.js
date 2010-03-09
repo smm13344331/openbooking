d7f39c8a613dd98fdb6a44bcb3c39ea62829536e
maidenfan <maidenfan@77e6e1be-d5c8-11de-9f34-657d07d5c07a>
2010-03-09 15:55:57 +0000

    Clear-out and re-sort

--- trunk/javascript/common.js
+++
@@ -1,8 +0,0 @@
-
-    /** JAVASCRIPT -- Common functions **/
-    
-function hide_element(id) {
-	new Effect.Fade(id, {duration: 0.6});
-    //document.getElementById(id).style.display = 'none';
-}
-
